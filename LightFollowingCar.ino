213486c00f3cc22fcbe52267462b07644b4ca72d
NitroShake <63812175+NitroShake@users.noreply.github.com>
2024-03-17 15:37:04 +0000

    Made indentations consistent

--- LightFollowingCar.ino
+++ LightFollowingCar.ino
@@ -8,15 +8,15 @@ class PidController {
     double lastTime = 0;
 
   public: PID(double targetValue, double Kp, double Ki, double Kd, double offset, double lowPassFilterGain, double maxOutput, double minOutput) {
-		this->targetValue = targetValue;
-		this->proportionalGain = Kp;
-		this->integralGain = Ki;
-		this->derivativeGain = Kd;
-		this->offset = offset;
-		this->lowPassFilterGain = lowPassFilterGain;
-		this->max = maxOutput;
-		this->min = minOutput;
-	}
+    this->targetValue = targetValue;
+    this->proportionalGain = Kp;
+    this->integralGain = Ki;
+    this->derivativeGain = Kd;
+    this->offset = offset;
+    this->lowPassFilterGain = lowPassFilterGain;
+    this->max = maxOutput;
+    this->min = minOutput;
+  }
 
   public: double calculateOutput(double currentInput) {
     //calculate the change in time since function was last called
@@ -27,31 +27,31 @@ class PidController {
 
     //calculate error + proportional
     double error = currentInput - targetValue;
-		double proportional = proportionalGain * error;
+    double proportional = proportionalGain * error;
 
     //calculate accumulated error, clamp it, then calculate integral
-		totalError += error * deltaTime;
+    totalError += error * deltaTime;
     //clamp values are calculated outside before constrain (documentation says don't do operations inside constrain)
     double minClamp = (min / integralGain) / 1.3;
     double maxClamp = (max / integralGain) / 1.3;
-		totalError = constrain(totalError, minClamp, maxClamp);
-		double integral = integralGain * totalError;
+    totalError = constrain(totalError, minClamp, maxClamp);
+    double integral = integralGain * totalError;
 
     //calculate derivative
     double derivative = (error - previousError) / deltaTime;
     //apply low pass filter
-		derivative = (lowPassFilterGain * previousDerivative) + ((1 - lowPassFilterGain) * (derivative));
+    derivative = (lowPassFilterGain * previousDerivative) + ((1 - lowPassFilterGain) * (derivative));
 
     //update "previous" vars to prepare for when this function next runs
     previousDerivative = derivative;
     previousError = error;
 
     //apply derivative gain to get final derivative value
-		derivative = derivative * derivativeGain;
+    derivative = derivative * derivativeGain;
 
     //calculate sum of p+i+d, clamp it, then apply offset, then return value
     double pid = proportional + integral + derivative;
-		return offset + constrain(pid, min, max);
+    return offset + constrain(pid, min, max);
   }
 };
 
