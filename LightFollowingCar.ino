96d79ac12cc14faa0666cbd29b5b02126751ae63
NitroShake <cameronskerry134@gmail.com>
2024-04-18 14:37:55 +0100

    Threshold/turn/travel

--- LightFollowingCar.ino
+++ LightFollowingCar.ino
@@ -6,7 +6,7 @@ class PidController {
     double totalError = 0;
     double lastTime = 0;
 
-  public: PID(double targetValue, double Kp, double Ki, double Kd, double offset, double lowPassFilterGain, double maxOutput, double minOutput) {
+  public: PidController(double targetValue, double Kp, double Ki, double Kd, double offset, double lowPassFilterGain, double maxOutput, double minOutput) {
     this->targetValue = targetValue;
     this->proportionalGain = Kp;
     this->integralGain = Ki;
@@ -31,6 +31,7 @@ class PidController {
     //calculate accumulated error, clamp it, then calculate integral
     totalError += error * deltaTime;
     //clamp values are calculated outside before constrain (documentation says don't do operations inside constrain)
+    //clamp values are arbitrary btw, feel free to change maybe if doesn't work well
     double minClamp = (min / integralGain) / 1.3;
     double maxClamp = (max / integralGain) / 1.3;
     totalError = constrain(totalError, minClamp, maxClamp);
@@ -52,14 +53,61 @@ class PidController {
     double pid = proportional + integral + derivative;
     return offset + constrain(pid, min, max);
   }
+
+  public: void reset() {
+    totalError = 0;
+    lastTime = micros();
+  }
 };
 
-void setup() {
-  // put your setup code here, to run once:
+PidController travelPid = PidController(
+  1,1,1,1,1,1,0,255
+);
 
+PidController turnPid = PidController(
+  1,1,1,1,1,1,0,255
+);
+
+void travel(int backward, int forward) {
+  int imbalance = forward - backward;
+  double motorOutput = travelPid.calculateOutput(imbalance);
+  //analogWrite(TRAVELSLOT, motorOutput);
+}
+
+void turn(int left, int right) {
+  int imbalance = right - left;
+  double motorOutput = turnPid.calculateOutput(imbalance);
+  //analogWrite(TURNSLOT, motorOutput);
+}
+
+void stop() {
+  //analogWrite(TRAVELSLOT, 0); //0, or whatever neutral is
+}
+
+void setup() {
+  
+    
 }
 
 void loop() {
+  //TODO: READ THE VALUES
+  int forward;
+  int backward;
+  int left;
+  int right;
 
+  //calculate threshold. If any lightsensor is above the threshold, run turn/travel
+  int threshold = ((forward + backward + left + right) / 4) + 50;
+  if (backward > threshold || forward > threshold || left > threshold || right > threshold) {
+    turn(left, right);
+    travel(backward, forward);
+  }
+  //if no lightsensors above threshold, stop the vehicle and reset the PIDs
+  else {
+    stop();
+    travelPid.reset();
+    turnPid.reset();
+  }
+  delay(50);
 }
 
